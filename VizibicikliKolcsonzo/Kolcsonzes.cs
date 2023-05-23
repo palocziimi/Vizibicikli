0a7d5ffed6510cef859081686bd4627f3c6ffc6f
Pálóczi Imre <palocziimi@gmail.com>
2023-05-23 20:02:15 +0200

    Add project files.

---
+++ VizibicikliKolcsonzo/Kolcsonzes.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VizibicikliKolcsonzo
+{
+    public class Kolcsonzes
+    {
+        string nev;
+        char jazon;
+        int eOra;
+        int ePerc;
+        int vOra;
+        int vPerc;
+
+        //!!!
+        //Bár könnyebbé teheti a munkát egy újabb konstruktor
+        //pld.ha sokszor kell CSV stringgel dolgozni, de OOP elveknek kevéssé felel meg.
+        //Ezt inkább az osztály használójának kellene kivül megoldania.
+        //Ezzel az osztályom erőssen fog függeni a CSV szerkezetétől.
+        //Ha változik a CSV szerkezet, akkor lehet átírni az osztályt is.
+        //Ezt a fajta függőséget (Dependency) nem szeretjük!
+        public Kolcsonzes(string csvSor)
+        {
+            var mezok = csvSor.Split(';');
+            this.nev = mezok[0];
+            this.jazon = mezok[1][0];
+            this.eOra = int.Parse(mezok[2]);
+            this.ePerc = int.Parse(mezok[3]);
+            this.vOra = int.Parse(mezok[4]);
+            this.vPerc = int.Parse(mezok[5]);
+        }
+        //!!! vége
+
+        public Kolcsonzes(string nev, char jazon, int eOra, int ePerc, int vOra, int vPerc)
+        {
+            this.nev = nev;
+            this.jazon = jazon;
+            this.eOra = eOra;
+            this.ePerc = ePerc;
+            this.vOra = vOra;
+            this.vPerc = vPerc;
+        }
+
+        public string Nev { get => nev; }
+        public char Jazon { get => jazon; }
+        public int EOra { get => eOra; }
+        public int EPerc { get => ePerc; }
+        public int VOra { get => vOra; }
+        public int VPerc { get => vPerc; }
+    }
+}
---
+++ VizibicikliKolcsonzo/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+namespace VizibicikliKolcsonzo
+{
+    internal class Program
+    {
+        static List<Kolcsonzes> kolcsonzesek = new List<Kolcsonzes>();
+        static void Main(string[] args)
+        {
+            //Hagyományos technika
+            StreamReader sr = new StreamReader("kolcsonzesek.txt");
+            sr.ReadLine();
+            while (!sr.EndOfStream)
+            {
+                var mezok = sr.ReadLine().Split(';');
+
+                Kolcsonzes uj = new Kolcsonzes(mezok[0],
+                                               char.Parse(mezok[1]),
+                                               int.Parse(mezok[2]),
+                                               int.Parse(mezok[3]),
+                                               int.Parse(mezok[4]),
+                                               int.Parse(mezok[5]));
+                kolcsonzesek.Add(uj);
+            }
+            sr.Close();
+
+            // LINQ + Foreach
+            //kolcsonzesek.Clear();
+
+            //foreach (var sor in File.ReadAllLines("kolcsonzesek.txt"))
+            //{
+            //    kolcsonzesek.Add(new Kolcsonzes(sor));
+            //}
+
+
+
+            //LINQ
+            //List<Kolcsonzes> masikLista = File.ReadAllLines("DataSource\\kolcsonzesek.txt").Skip(1).Select(x => new Kolcsonzes(x)).ToList();
+
+            //5.
+            Console.WriteLine($"5. feladat: Napi kölcsönzések száma: {kolcsonzesek.Count()}");
+
+            //6.
+            Console.Write("6. feladat: Kérek egy nevet: ");
+            string name = Console.ReadLine();
+            Console.WriteLine($"\n\t{name} kölcsönzései:");
+            if (kolcsonzesek.Where(x => x.Nev == name).ToList().Count == 0)
+            {
+                Console.WriteLine("\tNincs ilyen nevű kölcsönző.");
+            }
+            else
+            {
+                kolcsonzesek.Where(x => x.Nev == name).ToList().ForEach(x => Console.WriteLine($"\t{x.EOra}:{x.EPerc}-{x.VOra}:{x.VPerc}"));
+            }
+
+            //7.
+            Console.Write("7. feladat: Adjon meg egy időpontot óra:perc alakban: ");
+            string idopont = Console.ReadLine();
+            int idop = int.Parse(idopont.Split(':')[0]) * 60 + int.Parse(idopont.Split(':')[1]);
+            Console.WriteLine("\tA vízen lévő járművek: ");
+            kolcsonzesek.Where(x => (x.EOra * 60 + x.EPerc) < idop && (x.VOra * 60 + x.VPerc) > idop).ToList().ForEach(x => 
+                Console.WriteLine($"\t{x.EOra}:{x.EPerc}-{x.VOra}:{x.VPerc} : {x.Nev}"));
+
+            //8.
+
+
+
+
+        }
+    }
+}
